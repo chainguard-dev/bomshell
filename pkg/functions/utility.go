792281e7287357b19839819ef150b4f8f4f22275
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-07-12 01:19:58 -0600

    Add local orphan reconnect function
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/functions/utility.go
+++ pkg/functions/utility.go
@@ -40,3 +40,29 @@ func cleanEdges(nl *elements.NodeList) {
 
 	nl.Edges = newEdges
 }
+
+// reconnectOrphanNodes cleans the graph structure by reconnecting all
+// orphaned nodes to the top og the graph
+func reconnectOrphanNodes(nl *elements.NodeList) {
+	edgeIndex := map[string]struct{}{}
+	rootIndex := map[string]struct{}{}
+
+	for _, e := range nl.NodeList.Edges {
+		for _, t := range e.To {
+			edgeIndex[t] = struct{}{}
+		}
+	}
+
+	for _, id := range nl.NodeList.RootElements {
+		rootIndex[id] = struct{}{}
+	}
+
+	for _, n := range nl.NodeList.Nodes {
+		if _, ok := edgeIndex[n.Id]; !ok {
+			if _, ok := rootIndex[n.Id]; !ok {
+				nl.NodeList.RootElements = append(nl.NodeList.RootElements, n.Id)
+			}
+		}
+	}
+
+}
